b9711bc055687bcc89023ab52c71a90656d924a3
milanmatka <milanmatka777@gmail.com>
2025-06-14 01:07:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import fs from 'fs/promises';
-import svgr from '@svgr/rollup';
-// import svgr from 'vite-plugin-svgr'
+import svgr from 'vite-plugin-svgr'; // Use the Vite-compatible version
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            src: resolve(__dirname, 'src'),
-        },
+  resolve: {
+    alias: {
+      src: resolve(__dirname, 'src'),
     },
-    esbuild: {
-        loader: 'jsx',
-        include: /src\/.*\.jsx?$/,
-        exclude: [],
-    },
-    optimizeDeps: {
-        esbuildOptions: {
-            plugins: [
-                {
-                    name: 'load-js-files-as-jsx',
-                    setup(build) {
-                        build.onLoad(
-                            { filter: /src\\.*\.js$/ },
-                            async (args) => ({
-                                loader: 'jsx',
-                                contents: await fs.readFile(args.path, 'utf8'),
-                            })
-                        );
-                    },
-                },
-            ],
-        },
-    },
-
-
-    
-    // plugins: [react(),svgr({
-    //   exportAsDefault: true
-    // })],
-
-    plugins: [svgr(), react()],
-    base: '/Modernize-Vite'
+  },
+  plugins: [
+    svgr({
+      exportAsDefault: true,
+    }),
+    react(),
+  ],
+  base: './', // This ensures correct routing in Vercel deployment
 });
