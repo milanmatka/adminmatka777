704067087215b23e1321a4bfd2450ec71e5c322a
milanmatka <milanmatka777@gmail.com>
2025-06-13 20:20:16 +0000

    Add main application entry point with React and routing setup

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,47 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import svgr from 'vite-plugin-svgr';
+import fs from 'fs/promises';
+import svgr from '@svgr/rollup';
+// import svgr from 'vite-plugin-svgr'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      src: resolve(__dirname, 'src'),
+    resolve: {
+        alias: {
+            src: resolve(__dirname, 'src'),
+        },
     },
-  },
-  plugins: [
-    react(),
-    svgr({ exportAsDefault: true }),
-  ],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js$/, // Include .js files as JSX
-  },
-  base: './',
+    esbuild: {
+        loader: 'jsx',
+        include: /src\/.*\.jsx?$/,
+        exclude: [],
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [
+                {
+                    name: 'load-js-files-as-jsx',
+                    setup(build) {
+                        build.onLoad(
+                            { filter: /src\\.*\.js$/ },
+                            async (args) => ({
+                                loader: 'jsx',
+                                contents: await fs.readFile(args.path, 'utf8'),
+                            })
+                        );
+                    },
+                },
+            ],
+        },
+    },
+
+
+    
+    // plugins: [react(),svgr({
+    //   exportAsDefault: true
+    // })],
+
+    plugins: [svgr(), react()],
+    base: '/Modernize-Vite'
 });
